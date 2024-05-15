065ce210e3b32aa38116777b14b4d75ec70efc9d
Zion Koyl <hiszd1@gmail.com>
2024-05-15 01:57:03 -0400

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use hid_io_client::keyboard_capnp::keyboard::signal::volume::Command as VolumeCommand;
 use iced::theme;
 use iced::widget::{button, column, container, row, text};
 use iced::Application;
@@ -9,46 +10,39 @@ pub mod util;
 struct HidIoGui {
     count: u32,
     layer: u8,
-    volume: (String, u8, String),
-}
-
-#[derive(Debug, Clone, Copy)]
-enum HidState {
-    Loading,
-    Loaded(),
+    volume: (Option<VolumeCommand>, u8, Option<String>),
 }
 
 #[derive(Debug, Clone)]
 pub enum Message {
     Increment,
     Decrement,
-    LayerChanged(u8),
-    Volume(String, u8, String),
+    Hid(hid_client_stdout::Messages),
 }
 
-impl TryFrom<hid_client_stdout::Messages> for Message {
-    type Error = ();
-
-    fn try_from(msg: hid_client_stdout::Messages) -> Result<Self, Self::Error> {
-        match msg {
-            hid_client_stdout::Messages::Volume(c, v, a) => {
-                use hid_io_client::keyboard_capnp::keyboard::signal::volume::Command;
-                let cmd = match c {
-                    Command::Set => "Set".to_string(),
-                    Command::Inc => "Inc".to_string(),
-                    Command::Dec => "Dec".to_string(),
-                    Command::Mute => "Mute".to_string(),
-                    Command::UnMute => "UnMute".to_string(),
-                    Command::ToggleMute => "ToggleMute".to_string(),
-                };
-                Ok(Message::Volume(cmd, v.try_into().unwrap(), a.unwrap()))
-            }
-            hid_client_stdout::Messages::LayerChanged(l) => {
-                Ok(Message::LayerChanged(l.try_into().unwrap()))
-            }
-        }
-    }
-}
+// impl TryFrom<hid_client_stdout::Messages> for Message {
+//     type Error = ();
+//
+//     fn try_from(msg: hid_client_stdout::Messages) -> Result<Self, Self::Error> {
+//         match msg {
+//             hid_client_stdout::Messages::Volume(c, v, a) => {
+//                 use hid_io_client::keyboard_capnp::keyboard::signal::volume::Command;
+//                 let cmd = match c {
+//                     Command::Set => "Set".to_string(),
+//                     Command::Inc => "Inc".to_string(),
+//                     Command::Dec => "Dec".to_string(),
+//                     Command::Mute => "Mute".to_string(),
+//                     Command::UnMute => "UnMute".to_string(),
+//                     Command::ToggleMute => "ToggleMute".to_string(),
+//                 };
+//                 Ok(Message::Volume(cmd, v.try_into().unwrap(), a.unwrap()))
+//             }
+//             hid_client_stdout::Messages::LayerChanged(l) => {
+//                 Ok(Message::LayerChanged(l.try_into().unwrap()))
+//             }
+//         }
+//     }
+// }
 
 impl iced::Application for HidIoGui {
     type Theme = iced::Theme;
@@ -61,7 +55,7 @@ impl iced::Application for HidIoGui {
             Self {
                 count: 0,
                 layer: 0,
-                volume: ("".to_string(), 0, "".to_string()),
+                volume: (None, 0, None),
             },
             iced::Command::none(),
         )
@@ -72,32 +66,39 @@ impl iced::Application for HidIoGui {
     }
 
     fn update(&mut self, message: Self::Message) -> iced::Command<Self::Message> {
+        println!("Update: {:?}", message);
         match message {
             Message::Increment => {
                 if self.count != u32::MAX {
                     self.count += 1;
-                    println!("{}", self.count);
+                    println!("Inc: {}", self.count);
                 }
-                iced::Command::none()
             }
             Message::Decrement => {
                 if self.count != u32::MIN {
                     self.count -= 1;
-                    println!("{}", self.count);
+                    println!("Inc: {}", self.count);
                 }
-                iced::Command::none()
             }
-            Message::LayerChanged(l) => {
-                println!("Layer: {}", l);
-                self.layer = l;
-                iced::Command::none()
-            }
-            Message::Volume(c, v, a) => {
-                println!("Volume: {} {} {}", c, v, a);
-                self.volume = (c, v, a);
-                iced::Command::none()
+            Message::Hid(msg) => {
+                use hid_client_stdout::Messages;
+                match msg {
+                    Messages::LayerChanged(l) => {
+                        println!("Layer: {}", l);
+                        self.layer = l.try_into().unwrap();
+                    }
+                    Messages::Volume(c, v, a) => {
+                        println!("Volume: {:?} {} {:?}", c, v, a);
+                        let app = match a {
+                            Some(app) => Some(app.clone()),
+                            None => None,
+                        };
+                        self.volume = (Some(c), v.try_into().unwrap(), app);
+                    }
+                }
             }
         }
+        iced::Command::none()
     }
 
     fn subscription(&self) -> Subscription<Message> {
@@ -106,7 +107,7 @@ impl iced::Application for HidIoGui {
             keyboard::Key::Character("j") if modifiers.command() => Some(Message::Decrement),
             _ => None,
         });
-        let hid = subscriptions::hidio::hid_worker();
+        let hid = subscriptions::hidio::hid_worker().map(Message::Hid);
 
         iced::Subscription::batch(vec![kb, hid])
     }
@@ -130,18 +131,32 @@ impl iced::Application for HidIoGui {
         .spacing(10)
         .align_items(iced::Alignment::Center);
 
-        let info = row![
+        let mut cmd_str = "None".to_string();
+        if self.volume.0.is_some() {
+            cmd_str = hid_client_stdout::util::str_from_command(self.volume.0.clone().unwrap())
+        }
+
+        let volume = row![
             text("Command: "),
-            text(self.volume.0.clone()),
+            text(cmd_str),
             text("Volume: "),
             text(self.volume.1.to_string()),
             text("Application: "),
-            text(self.volume.2.clone())
+            text(match self.volume.2.clone() {
+                Some(app) => app,
+                None => "None".to_string(),
+            })
         ]
         .spacing(10)
+        .padding(20)
         .align_items(iced::Alignment::Center);
 
-        let col = column!(counter, info).padding(20);
+        let layer = row![text("Layer: "), text(self.layer),]
+            .spacing(10)
+            .padding(20)
+            .align_items(iced::Alignment::Center);
+
+        let col = column!(counter, volume, layer).padding(20);
 
         let cont = container(col)
             .width(iced::Length::Fill)
--- src/subscriptions/hidio.rs
+++ src/subscriptions/hidio.rs
@@ -10,49 +10,79 @@ pub enum State {
     Ready(BufReader<ChildStdout>),
 }
 
-pub fn hid_worker() -> Subscription<crate::Message> {
-    subscription::channel("hidio", 100, |mut output| async move {
-        let mut state = State::Starting;
+pub fn hid_worker() -> Subscription<hid_client_stdout::Messages> {
+    struct HidWorker;
 
-        loop {
-            match &mut state {
-                State::Starting => {
-                    let cmd = match std::process::Command::new("journalctl")
-                        .arg("-fu")
-                        .arg("hid-io")
-                        .stdout(Stdio::piped())
-                        .spawn()
-                    {
-                        Ok(cmd) => cmd,
-                        Err(_) => {
-                            continue;
-                        }
-                    };
-                    let stdout = BufReader::new(
-                        cmd.stdout
-                            .ok_or_else(|| {
-                                Error::new(ErrorKind::Other, "Could not capture standard output.")
-                            })
-                            .unwrap(),
-                    );
-                    state = State::Ready(stdout);
-                }
-                State::Ready(reader) => {
-                    let lines = reader.lines();
-                    for line in lines {
-                        let line = line.unwrap();
-                        let msg = match Messages::try_from(line.as_str()) {
-                            Ok(msg) => msg,
+    subscription::channel(
+        std::any::TypeId::of::<HidWorker>(),
+        100,
+        |mut output| async move {
+            let mut state = State::Starting;
+            let mut ready = false;
+
+            loop {
+                match &mut state {
+                    State::Starting => {
+                        let cmd = match std::process::Command::new("journalctl")
+                            .arg("-fu")
+                            .arg("hid-io-ergoone")
+                            .stdout(Stdio::piped())
+                            .spawn()
+                        {
+                            Ok(cmd) => cmd,
                             Err(_) => {
-                                println!("Error: {}", line);
-                                return;
+                                continue;
                             }
                         };
-                        output.send(crate::Message::try_from(msg).unwrap()).await;
+                        let stdout = BufReader::new(
+                            cmd.stdout
+                                .ok_or_else(|| {
+                                    Error::new(
+                                        ErrorKind::Other,
+                                        "Could not capture standard output.",
+                                    )
+                                })
+                                .unwrap(),
+                        );
+                        state = State::Ready(stdout);
                     }
-                    continue;
+                    State::Ready(reader) => {
+                        let lines = reader.lines();
+                        for line in lines {
+                            let mut line = line.unwrap();
+                            let end = line.find("]:");
+                            if end.is_some() {
+                                line = line[end.unwrap() + 3..].to_string();
+                            }
+                            if ready {
+                                let msg = match Messages::try_from(line.as_str()) {
+                                    Ok(msg) => msg,
+                                    Err(_) => {
+                                        println!("Error: {}", line);
+                                        continue;
+                                    }
+                                };
+                                println!("TRANSPOSE: {} *** {:?}", line, msg);
+                                match output.send(msg).await {
+                                    Ok(_) => {
+                                        println!("sent");
+                                    }
+                                    Err(_) => {
+                                        continue;
+                                    }
+                                }
+                            } else {
+                                if line == "READY" {
+                                    ready = true;
+                                    println!("READY: {}", line);
+                                } else {
+                                    println!("!READY: {}", line);
+                                }
+                            }
+                        }
+                    }
                 }
             }
-        }
-    })
+        },
+    )
 }
